f808ad4ff1da66edfd6be074f7d9d10e91afd7d9
Deep <diphiwase0987@gmail.com>
2025-12-30 16:37:41 +0530

    role permissions without prisma, with auth from betterautn by using admins plugins

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -4,13 +4,15 @@ import {
 } from "@/components/delete-user-btn";
 import { ReturnButton } from "@/components/return-btn";
 import { auth } from "@/lib/auth";
-import prisma from "@/lib/db";
+// import prisma from "@/lib/db";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
+  const headersList = await headers();
+
   const session = await auth.api.getSession({
-    headers: await headers(),
+    headers: headersList,
   });
 
   if (!session) {
@@ -33,10 +35,24 @@ export default async function DashboardPage() {
     );
   }
 
-  const users = await prisma.user.findMany({
-    orderBy: {
-      name: "asc",
+  // const users = await prisma.user.findMany({
+  //   orderBy: {
+  //     name: "asc",
+  //   },
+  // });
+
+  // now same but with better auth admin plugins integrated
+  const { users } = await auth.api.listUsers({
+    headers: headersList,
+    query: {
+      sortBy: "name",
     },
+  });
+
+  const sortedByRole = users.sort((a, b) => {
+    if (a.role === "ADMIN" && b.role !== "ADMIN") return -1;
+    if (a.role !== "ADMIN" && b.role === "ADMIN") return 1;
+    return 0;
   });
 
   return (
@@ -63,7 +79,7 @@ export default async function DashboardPage() {
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
+            {sortedByRole.map((user) => (
               <tr key={user.id} className="border-b text-sm text-left">
                 <td className="px-4 py-2">{user.id.slice(0, 8)}</td>
                 <td className="px-4 py-2">{user.name}</td>
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,14 +7,25 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function ProfilePage() {
+  const headersList = await headers();
   const session = await auth.api.getSession({
-    headers: await headers(),
+    headers: headersList,
   });
 
   if (!session) {
     // return <p className="text-destructive">Unauthorized</p>;
     return redirect("/auth/login"); // if some how pass middleware/proxy, this will ensure to protect this route
   }
+
+  const FULL_POST_ACCESS = await auth.api.userHasPermission({
+    headers: headersList,
+    body: {
+      // userId: session.user.id, // you can use just pass headers instead of this, both works
+      permissions: {
+        posts: ["update", "delete"],
+      },
+    },
+  });
 
   return (
     <div className="px-8 py-16 container mx-auto max-w-5xl space-y-8">
@@ -30,9 +41,18 @@ export default async function ProfilePage() {
             <Link href={"/admin/dashboard"}>Admin Dashboard</Link>
           </Button>
         )}
+
+        <SignOutButton />
       </div>
 
-      <SignOutButton />
+      <div className="text-2xl font-bold">Permissions</div>
+
+      <div className="space-x-4">
+        <Button size={"sm"}>MANAGE OWN POSTS</Button>
+        <Button size={"sm"} disabled={!FULL_POST_ACCESS.success}>
+          MANAGE ALL POSTS
+        </Button>
+      </div>
 
       <pre className="text-sm overflow-clip">
         {JSON.stringify(session, null, 2)}
